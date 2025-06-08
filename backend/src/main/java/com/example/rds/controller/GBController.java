c88bfac6606eccaa038f2247008a14246b499765
ShoheiShimokawa <throwsh@gmail.com>
2025-06-08 21:43:10 +0900

    re add

---
+++ backend/src/main/java/com/example/rds/controller/GBController.java
@@ -0,0 +1,32 @@
+package com.example.rds.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/books")
+public class GBController {
+
+    @Value("${google.books.api.url}")
+    private String googleBooksApiUrl;
+
+    @Value("${google.books.api.key}")
+    private String apiKey;
+
+
+    @GetMapping
+    public ResponseEntity<?> searchBooks(@RequestParam String query) {
+        String url = googleBooksApiUrl + "?q=" + query + "&maxResults=40&key=" + apiKey;
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+        return ResponseEntity.ok(response.getBody());
+    }
+}
+
