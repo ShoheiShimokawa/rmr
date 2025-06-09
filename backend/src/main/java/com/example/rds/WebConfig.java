d868090b421049e22af5366cac29b99a8524c8af
ShoheiShimokawa <throwsh@gmail.com>
2025-06-09 12:15:09 +0900

    resulve 403

--- backend/src/main/java/com/example/rds/WebConfig.java
+++ backend/src/main/java/com/example/rds/WebConfig.java
@@ -1,20 +1,20 @@
-package com.example.rds;
+// package com.example.rds;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-     @Value("${frontend.origin}")
-    private String frontendOrigin;
-	@Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(frontendOrigin)
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
-    }
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
+//      @Value("${frontend.origin}")
+//     private String frontendOrigin;
+// 	@Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowedOrigins(frontendOrigin)
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE");
+//     }
 	
 	
-}
+// }
