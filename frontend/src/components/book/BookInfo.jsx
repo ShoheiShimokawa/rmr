0b82da91b0d13cbc8dc41ac48535fe3ea3df2fd0
ShoheiShimokawa <throwsh@gmail.com>
2025-07-05 01:10:58 +0900

    fix aspects

--- frontend/src/components/book/BookInfo.jsx
+++ frontend/src/components/book/BookInfo.jsx
@@ -5,7 +5,9 @@ export const BookInfo = ({ book, onClick }) => {
     <>
       {book && (
         <div className="flex">
-          <Book book={book} onClick={onClick} />
+          <div className="flex-shrink-0">
+            <Book book={book} onClick={onClick} />
+          </div>
           <div className="ml-2 mt-2 text-xs text-zinc-800 font-soft">
             <div className="font-soft">{book.title}</div>
             <div className="text-zinc-500 mt-2 text-xs font-soft">
